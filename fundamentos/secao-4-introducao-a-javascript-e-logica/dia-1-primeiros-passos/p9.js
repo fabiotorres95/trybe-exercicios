5c001cfeb8941284ef5cd2d0520c6ab45b3f2e1d
fabiotorres95 <engfabiotorres@outlook.com>
2022-09-21 20:03:12 -0300

    corrigido erro em programa 9

--- fundamentos/secao-4-introducao-a-javascript-e-logica/dia-1-primeiros-passos/p9.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica/dia-1-primeiros-passos/p9.js
@@ -2,7 +2,7 @@ let angle1 = 30;
 let angle2 = 60;
 let angle3 = 90;
 
-if (angle1 >= 180 || angle2 >= 180 || angle3 >= 180) {
+if (angle1 < 0 || angle2 < 0 || angle3 < 0) {
     console.log('ERRO! valores inválidos.');
 } else if (angle1 + angle2 + angle3 == 180) {
     console.log('Os ângulos escolhidos formam um triângulo.');
