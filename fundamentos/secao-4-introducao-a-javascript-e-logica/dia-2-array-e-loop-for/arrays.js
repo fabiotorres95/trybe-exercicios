65c5507bba95c2e97cfd06350c0c088f28b3841f
fabiotorres95 <engfabiotorres@outlook.com>
2022-09-22 18:28:58 -0300

    feitos pontos 1 e 2 dos exercícios

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica/dia-2-array-e-loop-for/arrays.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// Ponto 1
+// for (let number of numbers) {
+//     console.log(number);
+// }
+
+// Ponto 2
+let sum = 0;
+for (let number of numbers) {
+    sum += number;
+}
+console.log(sum);
