76c71abae34cc58f81ae11519b37f1e627d7ebed
fabiotorres95 <engfabiotorres@outlook.com>
2022-09-22 18:48:25 -0300

    feito os pontos 3 e 4 dos exercícios

--- fundamentos/secao-4-introducao-a-javascript-e-logica/dia-2-array-e-loop-for/arrays.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica/dia-2-array-e-loop-for/arrays.js
@@ -1,4 +1,4 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; //array para todos os exercícios
 
 // Ponto 1
 // for (let number of numbers) {
@@ -6,8 +6,28 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // Ponto 2
+// let sum = 0;
+// for (let number of numbers) {
+//     sum += number;
+// }
+// console.log(sum);
+
+// Ponto 3
+// let sum = 0;
+// for (let number of numbers) {
+//     sum  += number;
+// } 
+// console.log(sum / numbers.length)
+
+// Ponto 4
 let sum = 0;
+let average = 0;
 for (let number of numbers) {
-    sum += number;
+    sum  += number;
 }
-console.log(sum);
+average = sum / numbers.length;
+if (average > 20) {
+    console.log('valor maior que 20');
+} else {
+    console.log('valor menor ou igual a 20');
+}
