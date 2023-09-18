2394d5cc1b89e4bbc029838f75e6c551987ef322
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-18 16:34:54 -0300

    initial commit 2

---
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/exercicio3.py
@@ -0,0 +1,10 @@
+# Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+# n = 5
+
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# Dica: Python sabe multiplicar sequências! Por exemplo, 3 * 'bla' resulta em blablabla. Isso se aplica a listas também, caso você precise.
