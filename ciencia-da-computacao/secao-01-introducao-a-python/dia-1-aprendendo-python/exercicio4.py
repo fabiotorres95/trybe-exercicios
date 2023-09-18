10e6fc56a56c16cc5349e362b0cea309ef215318
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-18 17:24:59 -0300

    exercicio4 done

--- ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/exercicio4.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-1-aprendendo-python/exercicio4.py
@@ -1,1 +1,15 @@
 # Crie uma função que receba uma lista de nomes e retorne o nome com a maior quantidade de caracteres. Por exemplo, para ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"], o retorno deve ser "Fernanda".
+
+def biggest(user_list):
+  result = 0
+  index = 0
+  while index < len(user_list):
+    if len(user_list[index]) > len(user_list[result]):
+      result = index
+    index += 1
+  
+  print(user_list[result])
+
+user_input = input("Digite uma lista de nomes separados por espaço: ")
+names = user_input.split()
+biggest(names)
