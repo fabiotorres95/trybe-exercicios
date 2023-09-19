192f12c2c5c5402e8664c531a4daea0f5692fe74
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-19 16:18:11 -0300

    exercicio4 done

--- ciencia-da-computacao/secao-01-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio4.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio4.py
@@ -4,3 +4,36 @@
 # Python,0.23201856148491878
 # Java,0.23201856148491878
 # PHP,0.23201856148491878
+
+import json
+import csv
+
+try:
+  categories_list = []
+  with open('books.json') as file:
+    json_list = json.load(file)
+    for book in json_list:
+      categories_list.append(book['categories'])
+
+  count_categories = {}
+  for list in categories_list:
+    for category in list:
+      if not category in count_categories: count_categories[category] = 1
+      else: count_categories[category] += 1
+
+  for category in count_categories:
+    count_categories[category] = (count_categories[category] / len(categories_list)) * 100
+
+
+  with open('books.csv', 'w', encoding='utf8') as newFile:
+    writer = csv.writer(newFile)
+
+    headers = ["categoria", "porcentagem"]
+    writer.writerow(headers)
+
+    for category in count_categories:
+      row = [category, count_categories[category]]
+      writer.writerow(row)
+
+except FileNotFoundError:
+  print('arquivo books.json e/ou books.csv não encontrado')
