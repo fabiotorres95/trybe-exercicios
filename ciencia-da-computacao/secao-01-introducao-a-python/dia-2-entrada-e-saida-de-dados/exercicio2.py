d8b8b308732c1d22ec4056afba26b744c473c6db
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-19 14:23:58 -0300

    exercicio2 done

--- ciencia-da-computacao/secao-01-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio2.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio2.py
@@ -3,3 +3,26 @@
 # 🦜 Para embaralhar uma palavra faça: scrambled_word = "".join(random.sample(word, len(word)))
 
 # 🦜 O sorteio de uma palavra aleatória pode ser feito utilizando o método choice: random.choice(["word1", "word2", "word3"]) -> "word2".
+import random
+
+words_list = ["ABACAXI", "COMPUTADOR", "LOUCURA", "JABUTICABA", "ESPINGARDA"]
+goal = random.choice(words_list)
+print(''.join(random.sample(goal, len(goal))))
+
+user_input = ''
+tries = 1
+while tries <= 3:
+  print("")
+  print(f"Tentativa {tries}")
+  user_input = input("Adivinhe a palavra embaralhada: ").upper()
+
+  if user_input == goal:
+    print("VOCÊ ACERTOU! PARABÉNS!")
+    break
+
+  print("Palavra errada!")
+
+  tries += 1
+
+print("")
+print("FIM DE JOGO")
