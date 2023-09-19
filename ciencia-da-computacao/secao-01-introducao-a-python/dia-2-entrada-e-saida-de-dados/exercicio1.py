d65d9df705d36142639f3a9d9bf295c57921becf
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-19 14:02:24 -0300

    exercicio1 done

--- ciencia-da-computacao/secao-01-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio1.py
+++ ciencia-da-computacao/secao-01-introducao-a-python/dia-2-entrada-e-saida-de-dados/exercicio1.py
@@ -7,8 +7,17 @@
 # PE
 # P
 
-def create_name_stairs(name):
+def create_name_stairs(name: str):
+  letters_list = []
+  for letter in name: letters_list.append(letter)
+
   index = 1
   while index <= len(name):
-    print(name)
+    result = ''.join(letters_list)
+    print(result)
+    letters_list.pop()
+
     index += 1
+
+user_input = input("Digite seu nome: ")
+create_name_stairs(user_input)
