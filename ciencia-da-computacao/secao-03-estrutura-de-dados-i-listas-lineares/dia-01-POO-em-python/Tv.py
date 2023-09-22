2ae6ed0689282822809be13f1a05b46a1062c7a6
fabiotorres95 <engfabiotorres@outlook.com>
2023-09-22 19:24:35 -0300

    exercicio1 done

--- ciencia-da-computacao/secao-03-estrutura-de-dados-i-listas-lineares/dia-01-POO-em-python/Tv.py
+++ ciencia-da-computacao/secao-03-estrutura-de-dados-i-listas-lineares/dia-01-POO-em-python/Tv.py
@@ -1,2 +1,27 @@
 class Tv:
-    pass
+    def __init__(self, tamanho) -> None:
+        self.__volume = 50
+        self.__canal = 1
+        self.__tamanho = tamanho
+        self.__ligada = False
+
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        return instance
+
+    def aumentar_volume(self) -> None:
+        if self.__volume < 99:
+            self.__volume += 1
+
+    def diminuir_volume(self) -> None:
+        if self.__volume > 0:
+            self.__volume -= 1
+
+    def modificar_canal(self, canal_input) -> None:
+        if type(canal_input) is int and 1 <= canal_input <= 99:
+            self.__canal = canal_input
+        else:
+            raise ValueError('Canal inválido')
+
+    def ligar_desligar(self) -> None:
+        self.__ligada = not self.__ligada
